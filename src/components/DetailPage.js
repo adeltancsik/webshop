054f83f6cb88c8eb5851dad1423446c84eae9a79
weichichou <weichi.cc@gmail.com>
2019-10-30 22:23:42 +0100

    add homepage link to test wishlist state still works

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import WishlistBtn from "./WishlistBtn";
+import { Link } from 'react-router-dom';
 
 export default class DetailPage extends React.Component {
   render() {
     return (
       <div>
+        <Link to='/'>Homepage</Link>
         DetailPage
         {this.props.name}
         <button>Add to cart</button>
